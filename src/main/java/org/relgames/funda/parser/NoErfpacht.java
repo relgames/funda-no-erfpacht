0c563e6a8c93f088c77eefa3ec569b7e888ae538
Oleg Poleshuk <relgames@gmail.com>
2014-04-10 23:56:47 +0200

    Added simple parser

--- src/main/java/org/relgames/funda/parser/NoErfpacht.java
+++ src/main/java/org/relgames/funda/parser/NoErfpacht.java
@@ -1,10 +1,28 @@
 package org.relgames.funda.parser;
 
-/**
- * @author opoleshuk
- */
-public class NoErfpacht {
-    public static void main(String[] args) {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.List;
+
+public class NoErfpacht {
+    private static final Logger log = LoggerFactory.getLogger(NoErfpacht.class);
+
+    private static final String FUNDA_URL = "http://m.funda.nl/koop/amsterdam/200000-375000/85+woonopp/";
+
+    public static void main(String[] args) {
+        WebDriver driver = new HtmlUnitDriver();
+        driver.get(FUNDA_URL);
+
+        List<WebElement> elements = driver.findElements(By.cssSelector("a.prop-item"));
+        log.info("Loaded {} elements", elements.size());
+
+        for (WebElement element : elements) {
+            System.out.println(element);
+        }
     }
 }
