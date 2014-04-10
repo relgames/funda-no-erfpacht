4e7e0877b8b702cd8ac4163112bc9be020d5b57b
Oleg Poleshuk <relgames@gmail.com>
2014-04-10 23:01:43 +0200

    Added HtmlUnit

--- build.sbt
+++ build.sbt
@@ -3,3 +3,9 @@ name := "funda-no-erfpacht"
 version := "1.0"
 
 scalaVersion := "2.10.4"
+
+scalacOptions += "-target:jvm-1.7"
+
+libraryDependencies += "org.seleniumhq.webdriver" % "webdriver-selenium" % "0.9.7376"
+
+libraryDependencies += "org.seleniumhq.webdriver" % "webdriver-htmlunit" % "0.9.7376"
--- src/main/scala/FundParser.scala
+++ src/main/scala/FundParser.scala
@@ -1,8 +1,15 @@
+import org.openqa.selenium.By
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+
 /**
  * @author opoleshuk
  */
 object FundParser {
+  val FUNDA_URL = "http://m.funda.nl/koop/amsterdam/200000-375000/85+woonopp/"
+
   def main(args: Array[String]) {
-    println("hello")
+    val driver = new HtmlUnitDriver
+    driver.get(FUNDA_URL)
+    val links = driver.findElements(By.tagName("a"))
   }
 }
