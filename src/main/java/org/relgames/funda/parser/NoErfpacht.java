3bc9d9fa32d6c3975ec28ec472c516366bd5c89b
Oleg Poleshuk <relgames@gmail.com>
2014-04-12 21:38:12 +0200

    Added parallel processing

--- src/main/java/org/relgames/funda/parser/NoErfpacht.java
+++ src/main/java/org/relgames/funda/parser/NoErfpacht.java
@@ -11,31 +11,47 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NoErfpacht {
     private static final Logger log = LoggerFactory.getLogger(NoErfpacht.class);
 
     private static final String FUNDA_URL = "http://m.funda.nl/koop/amsterdam/200000-375000/85+woonopp/";
-    private static final String CACHE_FILE = "cache.dat";
+    private static final String CACHE_FILE = "cache/cache.dat";
 
     public static void main(String[] args) throws IOException {
         RecordManager recMan = RecordManagerFactory.createRecordManager(CACHE_FILE);
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+
         try {
-            Map<String, String> cache = recMan.hashMap("erfpacht");
+            final Map<String, String> cache = recMan.hashMap("erfpacht");
 
-            Document document = Jsoup.connect(FUNDA_URL).get();
+            for (int i=1;; i++) {
+                String fundaUrl = FUNDA_URL + "p" + i;
+                log.info("Processing {}", fundaUrl);
+                Document document = Jsoup.connect(fundaUrl).get();
 
-            List<Element> elements = document.select("a.prop-item");
-            log.info("Loaded {} elements", elements.size());
-
-            for (Element element : elements) {
-                String url = "http://m.funda.nl" + element.attr("href");
-                String situation = cache.get(url);
-                if (situation == null) {
-                    situation = groundSituation(url);
-                    cache.put(url, situation);
+                List<Element> elements = document.select("a.prop-item");
+                if (elements.isEmpty()) {
+                    break;
                 }
-                System.out.println(url + "," + situation);
+                log.debug("Loaded {} elements", elements.size());
+
+                for (Element element : elements) {
+                    final String url = "http://m.funda.nl" + element.attr("href");
+                    executorService.submit(new Runnable() {
+                        @Override
+                        public void run() {
+                            String situation = cache.get(url);
+                            if (situation == null) {
+                                situation = groundSituation(url);
+                                cache.put(url, situation);
+                            }
+                            System.out.println(url + "," + situation);
+                        }
+                    });
+                }
             }
         } finally {
             recMan.commit();
